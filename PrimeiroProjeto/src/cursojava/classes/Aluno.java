e11c91c66ef4a4e94ee4649c919981e3f3100696
ferraretto <ferraretto@CPD>
2020-09-28 17:01:44 -0300

    Implementação de atributos a classe aluno "Disciplina 1,2,3,4"

--- PrimeiroProjeto/src/cursojava/classes/Aluno.java
+++ PrimeiroProjeto/src/cursojava/classes/Aluno.java
@@ -15,10 +15,15 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 
+	private String disciplina1;
 	private double nota1;
+	private String disciplina2;
 	private double nota2;
+	private String disciplina3;
 	private double nota3;
+	private String disciplina4;
 	private double nota4;
+	
 	
 	
 	public Aluno() { /*Cria os dados na memória, sendo padrão do java*/
@@ -146,6 +151,40 @@ public class Aluno {
 		return nota4;
 	}
 
+	
+	
+	public String getDisciplina1() {
+		return disciplina1;
+	}
+
+	public void setDisciplina1(String disciplina1) {
+		this.disciplina1 = disciplina1;
+	}
+
+	public String getDisciplina2() {
+		return disciplina2;
+	}
+
+	public void setDisciplina2(String disciplina2) {
+		this.disciplina2 = disciplina2;
+	}
+
+	public String getDisciplina3() {
+		return disciplina3;
+	}
+
+	public void setDisciplina3(String disciplina3) {
+		this.disciplina3 = disciplina3;
+	}
+
+	public String getDisciplina4() {
+		return disciplina4;
+	}
+
+	public void setDisciplina4(String disciplina4) {
+		this.disciplina4 = disciplina4;
+	}
+
 	public void setNota4(double nota4) {
 		this.nota4 = nota4;
 	}
@@ -166,7 +205,7 @@ public class Aluno {
 
 	public String getAlunoAprovado2() {
 		double media = this.getMediaNota();
-		if (media >= 99){
+		if (media > 99){ /* hauhauhauhauhauahauh, só passa com cem abestado */
 			return "Aprovado";
 		}else{
 			return "Reprovado";
@@ -174,13 +213,16 @@ public class Aluno {
 		
 	}
 
+	
+	
 	@Override
 	public String toString() {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-				+ "]";
+				+ serieMatriculado + ", nota1=" + nota1 + ", disciplina1=" + disciplina1 + ", nota2=" + nota2
+				+ ", disciplina2=" + disciplina2 + ", nota3=" + nota3 + ", disciplina3=" + disciplina3 + ", nota4="
+				+ nota4 + ", disciplina4=" + disciplina4 + "]";
 	}
 
 	@Override
